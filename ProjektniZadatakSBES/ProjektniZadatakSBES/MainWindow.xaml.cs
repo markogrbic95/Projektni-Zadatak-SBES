57b21a74fa9649e1dbb99fc090779cceb19355d6
markogrbic95 <marko_grbic@hotmail.com>
2017-11-16 12:40:46 +0100

    Added project

---
+++ ProjektniZadatakSBES/ProjektniZadatakSBES/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektniZadatakSBES
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int activeButton = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameworkElement.StyleProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata
+            {
+                DefaultValue = FindResource(typeof(Window))
+            });
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void signInButton_Click(object sender, RoutedEventArgs e)
+        {
+            activeButton = 1;
+            registerButton.Background = new SolidColorBrush(Color.FromRgb(236, 240, 241));
+            registerButton.Foreground = new SolidColorBrush(Color.FromRgb(73, 64, 65));
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            activeButton = 0;
+            signInButton.Background = new SolidColorBrush(Color.FromRgb(236, 240, 241));
+            signInButton.Foreground = new SolidColorBrush(Color.FromRgb(73, 64, 65));
+        }
+
+        private void exitBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void exitBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Background = new SolidColorBrush(Color.FromRgb(227, 99, 79));
+            ((Button)sender).Foreground = new SolidColorBrush(Color.FromRgb(255,255,255));
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (((Button)sender).Name == "signInButton" && activeButton == 0)
+            {
+                ((Button)sender).Background = new SolidColorBrush(Color.FromRgb(236, 240, 241));
+                ((Button)sender).Foreground = new SolidColorBrush(Color.FromRgb(73, 64, 65));
+                return;
+            }
+            else if (((Button)sender).Name == "registerButton" && activeButton == 1)
+            {
+                ((Button)sender).Background = new SolidColorBrush(Color.FromRgb(236, 240, 241));
+                ((Button)sender).Foreground = new SolidColorBrush(Color.FromRgb(73, 64, 65));
+            }
+        }
+    }
+}
