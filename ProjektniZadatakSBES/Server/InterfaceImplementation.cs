6da69581b01936b673b3bb119c8f2362d2450c6d
saddsasda <saasdsda>
2017-11-16 15:19:29 +0100

    Login completed

--- ProjektniZadatakSBES/Server/InterfaceImplementation.cs
+++ ProjektniZadatakSBES/Server/InterfaceImplementation.cs
@@ -10,19 +10,69 @@ namespace Server
 {
     public class InterfaceImplementation : Interface
     {
+        public static Dictionary<string,User> registeredUsers = new Dictionary<string, User>();
         public bool ChangePassword(string username, string oldPassword, string newPassword)
         {
-            throw new NotImplementedException();
+            if (registeredUsers.ContainsKey(username))
+            {
+                if (registeredUsers[username].Password == oldPassword)
+                {
+                    registeredUsers[username].Password = newPassword;
+                    Console.WriteLine("Password has been successfully changed!");
+
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Old password is incorrect!");
+                    return false;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("User with {0} username does not exist!", username);
+                return false;
+            }
         }
 
         public bool Login(string username, string password)
         {
-            throw new NotImplementedException();
+            if (registeredUsers.ContainsKey(username))
+            {
+                if(registeredUsers[username].Password == password)
+                {
+                    registeredUsers[username].Logged = true;
+                    Console.WriteLine("{0} logged successfully!",username);
+
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password!");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username!");
+                return false;
+            }
         }
 
         public bool Logout(string username)
         {
-            throw new NotImplementedException();
+            if (registeredUsers[username].Logged)
+            {
+                registeredUsers[username].Logged = false;
+                Console.WriteLine("User {0} successfully logout!", username);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You must be logged!");
+                return false;
+            }
         }
 
         public bool Registration(string name, string surname, string email, string phoneNumber, string accNumber, string username, string password)
@@ -30,35 +80,6 @@ namespace Server
 
 
             return true;
-        }
-
-        public string ReadFile(string fileName)
-        {
-            string ret = "";
-
-            try
-            {
-                // Create an instance of StreamReader to read from a file.
-                // The using statement also closes the StreamReader.
-                using (StreamReader sr = new StreamReader(fileName))
-                {
-                    string line;
-                    // Read and display lines from the file until the end of 
-                    // the file is reached.
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                // Let the user know what went wrong.
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-
-            return ret;
         }
     }
 }
