7f26cd14626c017a865289c3b51e60da1e466daa
markogrbic95 <marko_grbic@hotmail.com>
2017-11-17 14:54:41 +0100

    Login Validation

--- ProjektniZadatakSBES/Server/Program.cs
+++ ProjektniZadatakSBES/Server/Program.cs
@@ -12,28 +12,7 @@ namespace Server
     public class Program
     {
         static void Main(string[] args)
-        {
-<<<<<<< HEAD
-=======
-
-            
-
-
-            InterfaceImplementation i = new InterfaceImplementation();
-            bool upitnik=i.AddGroup("grupa1", "sokiSole");
-            bool upitnik1 = i.AddGroup("grupa1", "Dex");
-            bool upitnik3 = i.AddGroup("grupa2", "sokiSole");
-            bool upitnik4= i.AddGroup("grupa1", "sokiSole");
-            /*
-            List<User> users = new List<User>();
-            users.Add(new User("Nenad", "Grini", "A", "1", "1", "1", "1"));
-            i.WriteFile(users);
-
-            i.ReadFile();
-            */
-            Console.ReadKey();
-
->>>>>>> cbb4f285e766827690ec8716caa87ca51aee56b3
+        {               
             NetTcpBinding binding = new NetTcpBinding();
             string address = "net.tcp://localhost:25001/InterfaceImplementation";
 
@@ -50,10 +29,6 @@ namespace Server
 
             Console.ReadLine();
             host.Close();
-<<<<<<< HEAD
-=======
-
->>>>>>> cbb4f285e766827690ec8716caa87ca51aee56b3
         }
     }
 }
